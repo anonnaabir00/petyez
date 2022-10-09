d33d87787ed8a4cf2b81d6cf7d53b867f49aa2da
Asaduzzaman Abir <anonnaabir00@gmail.com>
2022-10-09 23:16:23 +0600

    Route Updated

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostsController;
 use App\Http\Controllers\EcommerceController;
+use App\Http\Controllers\BreedsController;
 use App\Http\Controllers\ServicesController;
 use App\Http\Controllers\DoctorController;
 
@@ -29,7 +31,15 @@ Route::post('/v2/otp/send/', [AuthController::class, 'send_otp']);
 Route::post('/v2/otp/verify/', [AuthController::class, 'verify_otp']);
 
 // User Routes
+Route::get('/v2/user/list', [AuthController::class, 'all_users']);
 Route::get('/v2/user/{phone}', [AuthController::class, 'get_user']);
+
+// Breeds Routes
+Route::get('/v2/breeds/list', [BreedsController::class, 'all_breeds']);
+Route::get('/v2/breeds/{type}', [BreedsController::class, 'get_breeds_by_breed_type']);
+
+// Post Routes
+Route::get('/v2/posts/list', [PostsController::class, 'all_posts']);
 
 // Products Route
 
@@ -40,10 +50,19 @@ Route::post('/v2/products/add', [EcommerceController::class, 'insert_products'])
 Route::post('/v2/order/create', [EcommerceController::class, 'create_order']);
 Route::post('/v2/order/confirm', [EcommerceController::class, 'confirm_payment']);
 
-// Services Route
+// Services Experts Route
 
 Route::get('/v2/service/experts/list', [ServicesController::class, 'get_experts']);
 Route::post('/v2/service/experts/insert', [ServicesController::class, 'insert_experts']);
+
+// Services Route
+
+Route::get('/v2/service/list', [ServicesController::class, 'get_services']);
+Route::post('/v2/service/insert', [ServicesController::class, 'insert_services']);
+
+// Service Booking Route
+Route::post('/v2/service/book', [ServicesController::class, 'book_services']);
+Route::post('/v2/service/confirm', [ServicesController::class, 'confirm_payment']);
 
 // Doctors Route
 Route::get('/v2/doctors/list', [DoctorController::class, 'get_doctors']);
