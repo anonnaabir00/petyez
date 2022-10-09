cf6d4362ceb11c2a4e2e07703fcefd8133f3bcb5
Asaduzzaman Abir <anonnaabir00@gmail.com>
2022-10-09 23:15:28 +0600

    Services Controller Added

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Models\ServicesExpertsModel;
+use App\Models\ServiceListModel;
+use App\Models\ServiceBookingModel;
 use Illuminate\Http\Request;
+use Razorpay\Api\Api;
 
 class ServicesController extends Controller {
 
@@ -15,5 +18,52 @@ class ServicesController extends Controller {
         $body = $request->all();
         $experts = ServicesExpertsModel::create($body);
         return response()->json($experts);
+    }
+
+    public function get_services(){
+        $services = ServiceListModel::all();
+        return response()->json($services);
+    }
+
+    public function insert_services(Request $request){
+        $body = $request->all();
+        $services = ServiceListModel::create($body);
+        return response()->json($services);
+    }
+
+    public function book_services(Request $request){
+        $api_key = "rzp_test_t9it7RBLljtGZy";
+        $api_secret = 'hImmJ8sjhwsPmDOVGWL2uZbu';
+        $api = new Api($api_key, $api_secret);
+
+        $body = $request->all();
+        $amount = $body['total_price'] * 100;
+
+        $orderData = [
+            'amount'          => $amount,
+            'currency'        => 'INR'
+        ];
+        
+        $razorpayOrder = $api->order->create($orderData);
+        $razorpayOrderId = $razorpayOrder['id'];
+
+        $services = ServiceBookingModel::create($body);
+        
+        // add razorpay order id to service booking table
+        $services->razorpay_order_id = $razorpayOrderId;
+        $services->payment_status = 'Not Paid';
+        $services->save();
+
+        return response()->json($services);
+    }
+
+    public function confirm_payment(Request $request) {
+        $body = $request->all();
+        $services = ServiceBookingModel::where('razorpay_order_id', $body['razorpay_order_id'])->first();
+        $services->razorpay_payment_id = $body['razorpay_payment_id'];
+        $services->razorpay_signature = $body['razorpay_signature'];
+        $services->payment_status = 'Paid';
+        $services->save();
+        return response()->json($services);
     }
 }
